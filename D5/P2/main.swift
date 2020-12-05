5f47fc8183b780504fc552b9ec46065405998928
andreja110s <andreja.jeric1@gmail.com>
2020-12-05 14:41:27 +0100

    Day 5 Part 1 and Part 2

---
+++ D5/P2/main.swift
@@ -0,0 +1,96 @@
+import Foundation
+
+func CalculateRow(_ letter: Character, _ min: Int, _ max: Int) -> [Int]
+{
+  var minRow = min
+  var maxRow = max
+
+  if letter == "F"
+  {
+    maxRow = max - ((max-min)/2) - 1
+
+    return [minRow, maxRow]
+  }
+  else //if letter == "B"
+  {
+    minRow = min + ((max-min)/2) + 1
+
+    return [minRow, maxRow]
+  }
+}
+
+func CalculateColumn(_ letter: Character, _ min: Int, _ max: Int) -> [Int]
+{
+
+  var minColumn = min
+  var maxColumn = max
+
+  if letter == "L"
+  {
+    maxColumn = max - ((max-min)/2) - 1
+
+    return [minColumn, maxColumn]
+  }
+  else //if letter == "R"
+  {
+    minColumn = min + ((max-min)/2) + 1
+
+    return [minColumn, maxColumn]
+  }
+}
+
+func CalculateSeatId(_ partitioning: String) -> Int
+{
+  let partsOfPartitioning = Array(partitioning)
+
+  var minRow: Int = 0
+  var maxRow: Int = 127
+
+  var minColumn: Int = 0
+  var maxColumn: Int = 7
+
+  for index in 0...partsOfPartitioning.count-4
+  {
+    let newRowNmbs = CalculateRow(partsOfPartitioning[index], minRow, maxRow)
+    minRow = newRowNmbs[0]
+    maxRow = newRowNmbs[1]
+  }
+
+  for index in partsOfPartitioning.count-3...partsOfPartitioning.count-1
+  {
+    let newColumnNmbs = CalculateColumn(partsOfPartitioning[index], minColumn, maxColumn)
+    minColumn = newColumnNmbs[0]
+    maxColumn = newColumnNmbs[1]
+  }
+
+  return minRow * 8 + minColumn
+}
+
+func CalculateYourSeatId(_ arrayOfPartitions: [String]) -> Int
+{
+  var sortedSeatIds = arrayOfPartitions.map { CalculateSeatId($0) }
+  sortedSeatIds.sort()
+
+  for index in 1...sortedSeatIds.count-2
+  {
+    if (sortedSeatIds[index] == sortedSeatIds[index + 1] + 2 
+    || sortedSeatIds[index] == sortedSeatIds[index - 1] + 2)
+    {
+      return sortedSeatIds[index] - 1
+    }
+    else if (sortedSeatIds[index] == sortedSeatIds[index + 1] - 2
+    || sortedSeatIds[index] == sortedSeatIds[index - 1] - 2)
+    {
+      return sortedSeatIds[index] + 1
+    }
+  }
+
+  return 0
+}
+
+let path = Bundle.main.path(forResource: "input", ofType: "txt")
+let fileContent = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+
+let arrayOfPartitions = fileContent.split(separator: "\n").map { String($0) }
+
+print(CalculateYourSeatId(arrayOfPartitions))
