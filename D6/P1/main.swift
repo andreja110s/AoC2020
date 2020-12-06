c56a124f443a61c0196c1980a0db5042ce6fb8f6
andreja110s <andreja.jeric1@gmail.com>
2020-12-06 12:57:06 +0100

    Day 6 Part 1 and Part 2

---
+++ D6/P1/main.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+func StringToSet (_ input: String) -> Set<Character>
+{
+  return Set(Array(input))
+}
+
+func CalculateGroupYesAnswers(_ groupAnswers: String) -> Int
+{
+  let groupPersonAnswers = (groupAnswers.split(separator: "\n").map { String($0) }).map { Set(Array($0)) }
+  
+  var uniqueYesAnswers = groupPersonAnswers[0]
+
+  for personAnswers in groupPersonAnswers
+  {
+    uniqueYesAnswers = uniqueYesAnswers.union(personAnswers)
+  }
+
+  return uniqueYesAnswers.count
+}
+
+func SumOfCounts(_ arrayOfGroups: [String]) -> Int
+{
+  var sum = 0
+
+  for group in arrayOfGroups
+  {
+    sum += CalculateGroupYesAnswers(group)
+  }
+
+  return sum
+}
+
+let path = Bundle.main.path(forResource: "input", ofType: "txt")
+let fileContent = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+
+let arrayOfGroups = fileContent.components(separatedBy: "\n\n")
+
+print(SumOfCounts(arrayOfGroups))
